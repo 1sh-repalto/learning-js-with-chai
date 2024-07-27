a19e90ca4937b4e7bfbe48674a97110590247e8f
1sh-repalto <145578673+1sh-repalto@users.noreply.github.com>
2024-07-27 10:07:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,11 @@
+let num1 = "123abc";
+num1 = Number(num1);
+console.log(typeof num1);
+console.log(num1);
+//"123" -> Number() -> 33
+//"123abc" -> Number() -> NaN
+
+//true -> 1
+//false -> 0
+
+//String(), Boolean() ...
