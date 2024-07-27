c2d0fab23869b3952e4d42923732ee108a2ffc09
1sh-repalto <145578673+1sh-repalto@users.noreply.github.com>
2024-07-27 07:40:16 +0000

    varisbles and datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+// console.table prints values of an array in tabular from
+console.table([a, b, c]);
+
+//prefer not to use var because of issue in block scope and functional scope, instead use let
---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treats all the JS code as newer version
+
+//alert("...") -> won't work when using nodejs
+
+//documentation -> tc39.es (original) , mdn (by mozilla)
+
+//DATATYPES
+
+    //max value a number can hold is 2^1023 × (2 - 2-52). Values higher than this are replace with special vlaue "Infinity".
+    //Integer range -> -2^53 + 1 to 2^53 - 1
+
+    //string -> ""
+
+    // boolean -> true/false
+
+    //null -> value exists but has no "meaning", typeof null is object
+
+    //undefined -> value doesn't exist, typeof -> undefined
+
+    //symbol -> unique
